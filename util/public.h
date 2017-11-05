6976e96d48ae581dddb68e29341e4209613dd41d
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2017-11-05 02:44:44 -0300

    Added string generation functions

--- util/public.h
+++ util/public.h
@@ -2,6 +2,8 @@
 
 #define ___UTIL
 
+#include <stdarg.h>
+
 // file.c
 FILE * open_text_file(const char * file, char ** error);
 FILE * open_binary_file(const char * file, char ** error);
@@ -13,5 +15,7 @@ int write_data_to_file(FILE * fp, const void * data, unsigned length);
 char * trim_string(char * string);
 char * duplicate_string(const char * string);
 unsigned convert_string_to_number(const char * string, char ** error);
+char * generate_string(const char * fmt, ...);
+char * generate_string_from_varargs(const char * fmt, va_list varargs);
 
 #endif
--- util/string.c
+++ util/string.c
@@ -35,3 +35,23 @@ unsigned char get_hex_digit (char digit) {
   if ((digit >= 'a') && (digit <= 'f')) return digit - ('a' - 10);
   return 0;
 }
+
+char * generate_string (const char * fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  char * result = generate_string_from_varargs(fmt, ap);
+  va_end(ap);
+  return result;
+}
+
+char * generate_string_from_varargs (const char * fmt, va_list varargs) {
+  char testbuf[2];
+  va_list temp;
+  va_copy(temp, varargs);
+  int rv = vsnprintf(testbuf, 1, fmt, temp);
+  va_end(temp);
+  if (rv < 0) return NULL;
+  char * result = malloc(rv + 1);
+  vsnprintf(result, rv + 1, fmt, varargs);
+  return result;
+}
