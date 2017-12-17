87346c61603b01c56ec85acaa89aa481b9002021
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2017-12-17 03:37:49 -0300

    Defined some additional option flags

--- optparse/pubtypes.h
+++ optparse/pubtypes.h
@@ -8,9 +8,9 @@ typedef struct options {
   struct option_file {
     char * name;
     char * label;
-    unsigned direction:  2; // 0: source, 1: source + target, 2: target
-    unsigned method:     6;
     unsigned reference: 24;
+    unsigned method:     6;
+    unsigned direction:  2; // 0: source, 1: source + target, 2: target
   } * input_files;
   struct {
     char * opening;
@@ -31,13 +31,16 @@ typedef struct options {
   unsigned input_file_count;
   unsigned fragment_size;
   unsigned padding_value;
-  unsigned current_conversion_direction:   2; // initialize to 1
-  unsigned current_conversion_method:      6;
-  unsigned current_conversion_reference:  24; // initialize to 0xffffff
-  unsigned input_bsp_source:               1;
-  unsigned detect_fragment_permutation:    1;
-  unsigned disable_output_validations:     2; // 0: none, 1: sha1, 2: sha1 + length
-  unsigned fragmentation_parameters_given: 1; // validation flag
+  unsigned current_conversion_reference:            24; // initialize to 0xffffff
+  unsigned current_conversion_method:                6;
+  unsigned current_conversion_direction:             2; // initialize to 1
+  unsigned input_bsp_source:                         1;
+  unsigned detect_fragment_permutation:              1;
+  unsigned disable_output_validations:               2; // 0: none, 1: sha1, 2: sha1 + length
+  unsigned fragmentation_parameters_given:           1; // validation flag
+  unsigned output_selection_on_single_option:        1;
+  unsigned suppress_error_messages:                  1;
+  unsigned no_source_patches:                        1;
 } * Options;
 
 #endif
