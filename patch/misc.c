805af8ab0ebe58b06519864b06ddf07c09146b6a
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2017-12-31 01:42:11 -0300

    Stub patch module

---
+++ patch/misc.c
@@ -0,0 +1,5 @@
+#include "proto.h"
+
+signed char find_patch_method_by_name (const char * method_name) {
+  // ...
+}
---
+++ patch/public.h
@@ -0,0 +1,8 @@
+#ifndef ___PATCH
+
+#define ___PATCH
+
+// misc.c
+signed char find_patch_method_by_name(const char * method_name);
+
+#endif
