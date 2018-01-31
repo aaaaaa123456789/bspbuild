e061508c205b807ff912610f76fff13e2b0215e3
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2018-01-31 04:26:23 -0300

    main() refactor

--- main/main.c
+++ main/main.c
@@ -2,20 +2,17 @@
 
 int main (int argc, char ** argv) {
   Options options = parse_options(argv + 1, argc - 1);
-  if (options -> response) {
+  int exit_status = 1; // default to 1 for the errors below
+  if (options -> response)
     fprintf(stderr, "%s\n", options -> response);
-    destroy_options_object(options);
-    return 1;
-  } else if (options -> error_text) {
+  else if (options -> error_text)
     fprintf(stderr, "%s: error: %s\n", *argv, options -> error_text);
-    destroy_options_object(options);
-    return 1;
-  }
-  int exit_status = ((int (* [])(Options)) {
-    &normal_operation_mode,
-    &bsp_input_operation_mode,
-    &ips_output_operation_mode
-  })[options -> operation_mode](options);
+  else
+    exit_status = ((int (* [])(Options)) {
+      &normal_operation_mode,
+      &bsp_input_operation_mode,
+      &ips_output_operation_mode
+    })[options -> operation_mode](options);
   destroy_options_object(options);
   return exit_status;
 }
