e25b3dd3f6d2d4e708dfbc464da868750c80a6fd
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2018-02-08 06:36:30 -0300

    Simple error function

--- pbuilder/errorfn.c
+++ pbuilder/errorfn.c
@@ -26,7 +26,26 @@ void define_error_function (void) {
 }
 
 const char * simple_error_message_function (char ** message_lines, unsigned message_line_count) {
-  // ...
+  int * message_labels = malloc(sizeof(int) * message_line_count);
+  unsigned current;
+  for (current = 0; current < message_line_count; current ++)
+    if ((message_labels[current] = declare_numeric_local_for_codefile(builder_state -> codefile)) < 0) break;
+  if (current < message_line_count) {
+    free(message_labels);
+    return "could not declare numeric local";
+  }
+  for (current = 0; current < message_line_count; current ++) inst(INST_PRINT, ARGTYPE_NUMERIC_LOCAL, message_labels[current]);
+  inst(INST_EXIT, ARGTYPE_NAMED_REGISTER, builder_state -> registers.argument);
+  add_blank_line_to_codefile(builder_state -> codefile);
+  for (current = 0; current < message_line_count; current ++) {
+    add_declared_numeric_local_to_codefile(builder_state -> codefile, message_labels[current]);
+    if (!add_string_to_codefile(builder_state -> codefile, message_lines[current])) {
+      free(message_labels);
+      return "could not add error message strings to output";
+    }
+  }
+  free(message_labels);
+  return NULL;
 }
 
 const char * descriptive_error_message_function (char ** message_lines, unsigned message_line_count) {
