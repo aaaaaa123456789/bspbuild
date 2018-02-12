7f8b3b83a936ede57f09ad6234cf574608718082
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2018-02-12 16:40:50 -0300

    Some fixes in input file option handling

--- optparse/validate.c
+++ optparse/validate.c
@@ -17,9 +17,16 @@ void validate_options (Options options) {
       return;
     }
   fill_in_missing_options(options);
+  if ((options -> file_count_per_direction[DIRECTION_SOURCE_TARGET] == 1) && !(options -> file_count_per_direction[DIRECTION_TARGET])) {
+    options -> file_count_per_direction[DIRECTION_SOURCE_TARGET] = 0;
+    options -> file_count_per_direction[DIRECTION_TARGET] = 1;
+    options -> input_files[options -> input_file_count - 1].direction = DIRECTION_TARGET;
+  }
 }
 
 char * validate_input_file_options (Options options) {
+  if (!(options -> input_file_count)) return copy_string_for_options(options, "no input files");
+  if (options -> input_file_count == 1) return copy_string_for_options(options, "only one input file given");
   if (options -> file_count_per_direction[DIRECTION_SOURCE_TARGET]) return NULL;
   if (!(options -> file_count_per_direction[DIRECTION_TARGET])) return copy_string_for_options(options, "no target input files");
   if (!(options -> file_count_per_direction[DIRECTION_SOURCE])) return copy_string_for_options(options, "no source input files");
