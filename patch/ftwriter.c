87db15f074a67d6a35806e608afb2d19e1636cd2
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2018-04-14 10:34:51 -0300

    Document fragment permutation table structure

--- patch/ftwriter.c
+++ patch/ftwriter.c
@@ -1,5 +1,13 @@
 #include "proto.h"
 
+/*
+   Fragment permutation table format:
+   data stored in 32-bit words, top bit: command, 31 bits: fragment affected
+   top bit 1: clear fragment (1 word, payload indicates fragment ID)
+   top bit 0: copy/swap (2 words, first word payload indicates source, second word payload indicates target, second word command is 0 for copy, 1 for swap)
+   -1 ends list
+*/
+
 char * write_fragment_permutation_table_to_codefile (CodeFile codefile, const struct fragment_permutation_table * table, int reverse) {
   // ...
 }
