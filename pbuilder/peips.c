88a494d223e275ed4e0d49edfa28eb8b3a45307c
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2018-04-23 00:16:30 -0300

    Indentation

--- pbuilder/peips.c
+++ pbuilder/peips.c
@@ -1,14 +1,14 @@
 #include "proto.h"
 
 void define_apply_ips_patch_function (void) {
-	inst(INST_JUMP, loc("handle_loop"));
-	builder_declare_local("loop");
-	inst(INST_SEEK, reg(temp));
-	inst(INST_GETWORDINC, reg(temp), reg(argument));
-	inst(INST_IPSPATCH, reg(temp), reg(temp));
-	builder_declare_local("handle_loop");
-	inst(INST_GETWORDINC, reg(temp), reg(argument));
-	inst(INST_IFNE, reg(temp), imm(-1), loc("loop"));
-	inst(INST_GETWORD, reg(temp), reg(argument));
+  inst(INST_JUMP, loc("handle_loop"));
+  builder_declare_local("loop");
+  inst(INST_SEEK, reg(temp));
+  inst(INST_GETWORDINC, reg(temp), reg(argument));
+  inst(INST_IPSPATCH, reg(temp), reg(temp));
+  builder_declare_local("handle_loop");
+  inst(INST_GETWORDINC, reg(temp), reg(argument));
+  inst(INST_IFNE, reg(temp), imm(-1), loc("loop"));
+  inst(INST_GETWORD, reg(temp), reg(argument));
   inst(INST_TRUNCATE, reg(temp));
 }
