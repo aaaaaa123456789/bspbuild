747cc26778b5b34cf422be50e367086ed599fc35
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2018-05-01 14:48:00 -0300

    Use newly-declared constant

--- bsp/scrdata.c
+++ bsp/scrdata.c
@@ -16,10 +16,10 @@ void append_binary_file_to_script (const char * file) {
   char * error;
   FILE * fp = open_binary_file(file, &error);
   if (error) bsp_throw_error("%s", error);
-  char * buffer = mr_malloc(bsp_memory_region, 65536);
+  char * buffer = mr_malloc(bsp_memory_region, FILE_BUFFER_SIZE);
   unsigned rv;
   while (!feof(fp)) {
-    rv = fread(buffer, 1, 65536, fp);
+    rv = fread(buffer, 1, FILE_BUFFER_SIZE, fp);
     if ((!rv) && ferror(fp)) bsp_throw_error("could not read data from binary file %s", file);
     append_data_to_script(buffer, rv);
   }
