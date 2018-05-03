6f46f0de2bacd93203904b75e73a6b88b1bacb93
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2018-05-03 06:56:23 -0300

    Modified a default

--- optparse/main.c
+++ optparse/main.c
@@ -32,7 +32,7 @@ Options new_options_object (void) {
   Options result = mr_calloc(mr, sizeof(struct options));
   result -> memory_region = mr;
   result -> current_conversion_direction = DIRECTION_SOURCE_TARGET;
-  result -> current_conversion_method = 0; // optimization hint; not strictly needed
+  result -> current_conversion_method = METHOD_XOR_RLE;
   result -> current_conversion_reference = TARGET_TYPE_PREVIOUS;
   return result;
 }
